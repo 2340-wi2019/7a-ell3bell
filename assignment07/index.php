326a911af0edd3d1c07513c090bacf0b07b00766
ell3bell <58443565+ell3bell@users.noreply.github.com>
2020-02-02 12:02:18 -0600

    Add files via upload

---
+++ assignment07/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>assignment07</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <!--
+            Allegra French
+            assignment07
+            INFO_2340_WW
+            Thoendel
+            2.2.2020
+        -->
+
+    <?php 
+    
+    $food_name = "pulled pork";
+    $stars = "3";
+    $place = "the Zoo";
+    $fav_animal = "cat";
+    ?>
+
+    <h1>Good morning, Nebraska!</h>
+    <div>Last night I ate <?php echo $food_name; ?>.</div>
+    <div>While it was good, it wasn't perfect. I'd give it <?php echo $stars; ?> stars out of 4.</div>
+    <div>Tomorrow I plan to visit <?php echo $place; ?>.</div>
+    <div>My favorite animal is the <?php echo $fav_animal; ?>.</div>
+
+</body>
+
+</html>
